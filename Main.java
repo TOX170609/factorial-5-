94a0d9b4a1645798590fb8178b839190a45ac4ec
TOX170609 <TOX170609@mail.ru>
2020-03-22 12:42:46 +0500

    first

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int b=1;
+	int a=5;
+	for (int i=a;i>0;i--)
+    {b*=i;}
+	System.out.println(b);
+    }
+}
